d21203c92a7ccd5743ef59afa93c893d92259dc1
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-10-22 11:12:39 +0200

    Basic mongodb and graphql configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      context: ({ req }) => ({ headers: req.headers }),
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
