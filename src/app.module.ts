65db23b4e7f2df4b2329f1880efc06c6ce188bfe
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-11-07 21:33:06 +0100

    Logging interceptor for debugging

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import 'dotenv/config';
-
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 @Module({
   imports: [
     GraphQLModule.forRoot({
@@ -20,6 +21,11 @@ import 'dotenv/config';
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
 bootstrap();
